3708d4a704b0eaf60c893300e9466198835cb130
Prashant Naik <bhimanprash16@gmail.com>
2026-06-26 20:57:40 +0530

    Deployment setup and env added

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+// const express = require("express");
+// const http = require("http");
+// const cors = require("cors");
+
+// const { Server } = require("socket.io");
+
+// const registerSockets = require("./socket/index");
+
+// const app = express();
+
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
+
+// registerSockets(io);
+
+// server.listen(5000, () => {
+//   console.log("Signaling server running");
+// });
+
+// Deployment code
+
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
@@ -15,11 +43,14 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
 registerSockets(io);
 
-server.listen(5000, () => {
-  console.log("Signaling server running");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Signaling server running on port ${PORT}`);
 });
